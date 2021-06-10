2d5e36dd2a95b83627cf27c1f3397826fec0c2c1
Szabó István (János) <szabo.istvan.janos@fonixit.hu>
2021-06-10 19:54:28 +0200

    as we discussed

--- paint.js
+++ paint.js
@@ -1,43 +1,65 @@
-let theImages = new Array() 
+let theImages = [];
 
-        theImages[0] = "stevejohnson_0.jpg"
-        theImages[1] = "stevejohnson_1.jpg"
-        theImages[2] = "stevejohnson_2.jpg"
-        theImages[3] = "stevejohnson_3.jpg"
-        theImages[4] = "stevejohnson_4.jpg"
-        theImages[5] = "stevejohnson_5.jpg"
-        theImages[6] = "stevejohnson_6.jpg"
-        theImages[7] = "stevejohnson_7.jpg"
-        theImages[8] = "stevejohnson_8.jpg"
-        theImages[9] = "stevejohnson_9.jpg"
-        theImages[10] = "stevejohnson_10.jpg"
-        theImages[11] = "stevejohnson_11.jpg"
-        theImages[12] = "stevejohnson_12.jpg"
-        theImages[13] = "stevejohnson_13.jpg"
-        theImages[14] = "stevejohnson_14.jpg"
-        theImages[15] = "stevejohnson_15.jpg"
-        theImages[16] = "stevejohnson_16.jpg"
-        theImages[17] = "stevejohnson_17.jpg"
-        theImages[18] = "stevejohnson_18.jpg"
-        theImages[19] = "stevejohnson_19.jpg"
-        theImages[20] = "stevejohnson_20.jpg"
-        theImages[21] = "stevejohnson_21.jpg"
-        theImages[22] = "stevejohnson_22.jpg"
-        theImages[23] = "stevejohnson_23.jpg"
-        theImages[24] = "stevejohnson_24.jpg"
-        theImages[25] = "stevejohnson_25.jpg"
-       
-       
+theImages[0] = "stevejohnson_0.jpg";
+theImages[1] = "stevejohnson_1.jpg";
+theImages[2] = "stevejohnson_2.jpg";
+theImages[3] = "stevejohnson_3.jpg";
+theImages[4] = "stevejohnson_4.jpg";
+theImages[5] = "stevejohnson_5.jpg";
+theImages[6] = "stevejohnson_6.jpg";
+theImages[7] = "stevejohnson_7.jpg";
+theImages[8] = "stevejohnson_8.jpg";
+theImages[9] = "stevejohnson_9.jpg";
+theImages[10] = "stevejohnson_10.jpg";
+theImages[11] = "stevejohnson_11.jpg";
+theImages[12] = "stevejohnson_12.jpg";
+theImages[13] = "stevejohnson_13.jpg";
+theImages[14] = "stevejohnson_14.jpg";
+theImages[15] = "stevejohnson_15.jpg";
+theImages[16] = "stevejohnson_16.jpg";
+theImages[17] = "stevejohnson_17.jpg";
+theImages[18] = "stevejohnson_18.jpg";
+theImages[19] = "stevejohnson_19.jpg";
+theImages[20] = "stevejohnson_20.jpg";
+theImages[21] = "stevejohnson_21.jpg";
+theImages[22] = "stevejohnson_22.jpg";
+theImages[23] = "stevejohnson_23.jpg";
+theImages[24] = "stevejohnson_24.jpg";
+theImages[25] = "stevejohnson_25.jpg";
+
 let j = 0
 let p = theImages.length;
-let preBuffer = new Array()
-for (i = 0; i < p; i++){
-   preBuffer[i] = new Image()
-   preBuffer[i].src = theImages[i]
+let preBuffer = [];
+for (let i = 0; i < p; i++) {
+    preBuffer[i] = new Image()
+    preBuffer[i].src = theImages[i]
 }
 
-let whichImage = Math.round(Math.random()*(p-1));
+let generated = [];
 
-function showImage(){
-document.write('<img src="'+theImages[whichImage]+'"'+'alt="A painting of Steve Johnson"'+'>');
+
+function initImage() {
+    generated = [];
+}
+
+function showImage() {
+    let whichImage;
+    let newImage;
+    do {
+        whichImage = Math.round(Math.random() * (p - 1));
+        newImage = true;
+        for (let i = 0; i < generated.length; i++) {
+            if (generated[i] === whichImage) {
+                newImage = false;
+                break;
+            }
+        }
+        if (newImage) {
+            console.log("new: " + whichImage);
+            generated.push(whichImage);
+        } else {
+            console.log("already was generated: " + whichImage);
+        }
+    } while (!newImage);
+    document.write('<img src="' + theImages[whichImage] + '" alt="A painting of Steve Johnson"' + '>');
 }

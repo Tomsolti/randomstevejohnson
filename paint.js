7d1e7218570700a9ecd4aad5cadaea3d1ed8fda1
Tomsolti <tomsolti.dev@gmail.com>
2021-06-18 00:20:50 +0200

    Array sampled

--- paint.js
+++ paint.js
@@ -1,52 +1,19 @@
+//Images array and global vars
 let theImages = [];
-
-theImages[0] = "stevejohnson_0.jpg";
-theImages[1] = "stevejohnson_1.jpg";
-theImages[2] = "stevejohnson_2.jpg";
-theImages[3] = "stevejohnson_3.jpg";
-theImages[4] = "stevejohnson_4.jpg";
-theImages[5] = "stevejohnson_5.jpg";
-theImages[6] = "stevejohnson_6.jpg";
-theImages[7] = "stevejohnson_7.jpg";
-theImages[8] = "stevejohnson_8.jpg";
-theImages[9] = "stevejohnson_9.jpg";
-theImages[10] = "stevejohnson_10.jpg";
-theImages[11] = "stevejohnson_11.jpg";
-theImages[12] = "stevejohnson_12.jpg";
-theImages[13] = "stevejohnson_13.jpg";
-theImages[14] = "stevejohnson_14.jpg";
-theImages[15] = "stevejohnson_15.jpg";
-theImages[16] = "stevejohnson_16.jpg";
-theImages[17] = "stevejohnson_17.jpg";
-theImages[18] = "stevejohnson_18.jpg";
-theImages[19] = "stevejohnson_19.jpg";
-theImages[20] = "stevejohnson_20.jpg";
-theImages[21] = "stevejohnson_21.jpg";
-theImages[22] = "stevejohnson_22.jpg";
-theImages[23] = "stevejohnson_23.jpg";
-theImages[24] = "stevejohnson_24.jpg";
-theImages[25] = "stevejohnson_25.jpg";
-
-let j = 0
-let p = theImages.length;
-let preBuffer = [];
-for (let i = 0; i < p; i++) {
-    preBuffer[i] = new Image()
-    preBuffer[i].src = theImages[i]
+for (let i = 0; i <= 25; i++) {
+    theImages[i] = "stevejohnson_" + i + ".jpg";
 }
 
+let l = theImages.length;
 let generated = [];
 
 
-function initImage() {
-    generated = [];
-}
-
+//Function to pick a random image and display it
 function showImage() {
     let whichImage;
     let newImage;
     do {
-        whichImage = Math.round(Math.random() * (p - 1));
+        whichImage = Math.round(Math.random() * (l - 1));
         newImage = true;
         for (let i = 0; i < generated.length; i++) {
             if (generated[i] === whichImage) {
@@ -61,5 +28,5 @@ function showImage() {
             console.log("already was generated: " + whichImage);
         }
     } while (!newImage);
-    document.write('<img src="' + theImages[whichImage] + '" alt="A painting of Steve Johnson"' + '>');
+    document.write('<img src="' + theImages[whichImage] + '" alt="A random painting of Steve Johnson"' + '>');
 }
